5bda3a685ab9503f4577278d7b87e5952e673fa9
Benjamin Klüglein <scheibenkaes@googlemail.com>
2013-12-15 16:28:38 +0100

    init

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject org.scheibenkaes/backdoor "0.1.0-SNAPSHOT"
+  :description "Friend backend for persona"
+  :url "http://scheibenkaes.org"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [http-kit "2.1.13"]]
+  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]
+                   :dependencies [[midje "1.5.1"]
+                                  [http-kit.fake "0.2.0"]]}})
---
+++ test/backdoor/core_test.clj
@@ -0,0 +1,7 @@
+(ns backdoor.core-test
+  (:require [clojure.test :refer :all]
+            [backdoor.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
